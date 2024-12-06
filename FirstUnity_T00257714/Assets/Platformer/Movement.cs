84386cf2cf9e94aff4fbb33be0988cb689c04092
Patryk Graszk <patryk.graszk@students.ittralee.ie>
2024-12-06 23:04:23 +0000

    Signed-off-by: Patryk Graszk <patryk.graszk@students.ittralee.ie>
    Created block dodger game

--- FirstUnity_T00257714/Assets/Platformer/Movement.cs
+++ FirstUnity_T00257714/Assets/Platformer/Movement.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    Rigidbody rb;
+    float turnSpeed = 200;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,23 @@ public class Movement : MonoBehaviour
 
         if (Input.GetKey(KeyCode.S))
         {
+            transform.position -= transform.forward * Time.deltaTime;
+        }
 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down, turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position += transform.up * Time.deltaTime;
+            rb.AddForce(400 * transform.up);
         }
     }
 }
